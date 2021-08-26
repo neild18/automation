27199d25c2d2b27ec2ba4d0694ae8e30abf5c01e
neild18 <89464043+neild18@users.noreply.github.com>
2021-08-26 12:17:04 +0100

    Update keyword_filter_script.py

--- keyword_filter_script.py
+++ keyword_filter_script.py
@@ -8,9 +8,11 @@ import time
 def main():
 	st.title("Gaming Keyword Filter Tool")
 	st.text('Filter out all the irrelevant keywords from your downloads from your favourite keyword research tools!')
-	st.text('Make sure that the CSV contains a column titled \'Keyword\'. \'KEYWORD\' won\'t work. The keyword column is essential, otherwise the file won\'t work.')
+	st.subheader('What does this tool do?')
+	st.text('1\. It will add new columns for \'Intent\', \'Games\',  and for gaming related \'Modifiers\'.')
+	st.text('2\. It will create a column called \'Filter\' with Positive or Negative values. Positive being relevant keywords and Negative being irrelevant keywords.')
+	st.warning('Make sure that the CSV contains a column titled \'Keyword\'. \'KEYWORD\' won\'t work. The keyword column is essential, otherwise the file won\'t work.')
 	st.text('Once uploaded, go grab a brew (preferably something soft) while the file is being processed.')
-	
 	
 	uploaded_file = st.file_uploader("Choose a file")
 	if uploaded_file is not None:
