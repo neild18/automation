3020dfadb14a1b0be0648f3616cf2af7391bf43a
neild18 <89464043+neild18@users.noreply.github.com>
2021-09-08 16:44:05 +0100

    Update keyword_filter_script.py

--- keyword_filter_script.py
+++ keyword_filter_script.py
@@ -16,7 +16,15 @@ def main():
 	
 	uploaded_file = st.file_uploader("Choose a file")
 	if uploaded_file is not None:
-		df = pd.read_csv(uploaded_file)
+		def read_file(uploaded_file):
+		    csv = re.search(r'csv$', path)
+		    excel = re.search(r'xlsx$', path)
+		    if excel:
+		        df = pd.read_excel(path) # Default is zero
+		    elif csv: # Assuming "csv". You can make it explicit
+		        df = pd.read_csv(path)
+		    return df
+		df = read_file(uploaded_file)
 		df.columns= df.columns.str.lower()
 		df['keyword'] = df['keyword'].astype(str)
 		def intent(keyword=""): 
